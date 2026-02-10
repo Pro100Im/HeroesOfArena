1562b2496704eec8fff4da4e67f5897a168ab53b
Elite <pro100elite@gmail.com>
2026-02-10 13:42:37 +0100

    UI Toolkit menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Menu
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private UIDocument _mainMenuDoc;
+        
+        private Button _quickPlayButton;
+        private Button _testButton;
+        private Button _exitButton;
+
+        private void OnValidate()
+        {
+            if(_mainMenuDoc == null)
+                _mainMenuDoc = GetComponent<UIDocument>();
+        }
+
+        private void Awake()
+        {
+            var root = _mainMenuDoc.rootVisualElement;
+
+            _quickPlayButton = root.Q<Button>("QuickPlayButton");
+            _testButton = root.Q<Button>("TestButton");
+            _exitButton = root.Q<Button>("ExitButton");
+
+            _quickPlayButton.clickable.clicked += QuickPlay;
+            _testButton.clickable.clicked += Test;
+            _exitButton.clickable.clicked += Exit;
+        }
+
+        private void QuickPlay()
+        {
+
+        }
+
+        private void Test()
+        {
+            Debug.Log("Test button clicked!");
+        }
+
+        private void Exit()
+        {
+            Application.Quit();
+        }
+
+        private void OnDestroy()
+        {
+            _quickPlayButton.clickable.clicked -= QuickPlay;
+            _testButton.clickable.clicked -= Test;
+            _exitButton.clickable.clicked -= Exit;
+        }
+    }
+}
